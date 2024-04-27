d18bd07528d720e9b79c1f4923f4b0d9dbbb486a
Augusto Castilho Borges <augustocastilhoborges21@gmail.com>
2024-04-27 18:15:37 -0300

    Footer

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,9 @@
-const navbar = document.querySelector("data-navbar");
-const navToggler = document.querySelector("data-nav-toggler");
+'use strict';
 
-navToggler.addEventListener("click", function() {
+const navbar = document.querySelector("[data-navbar]");
+const navToggler = document.querySelector("[data-nav-toggler]");
+
+navToggler.addEventListener("click", function(){
     navbar.classList.toggle("active");
     this.classList.toggle("active");
 })
